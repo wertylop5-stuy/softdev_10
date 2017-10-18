17e4cd50b55319cca30b2585c99c80fd0b591940
Stanley Lin <slin55055@gmail.com>
2017-10-17 20:17:33 -0400

    work

--- stu_mean.py
+++ stu_mean.py
@@ -17,7 +17,7 @@ def get_grades(c, student_name):
 	ret = {}
 	for course in courses:
 		ret[course[0]] = course[1]
-	
+	print ret
 	return ret
 
 def get_average(c, student_name):
@@ -25,12 +25,21 @@ def get_average(c, student_name):
 	
 	avg = 0
 	for grade in courses.values():
+		print grade
 		avg += grade
 
 	avg /= len(courses.keys())
 	return avg
 
+def update_average(c, student_name):
+	avg = get_average(c, student_name)
+	
+	temp = formatted_select(c, ["id"], ["peeps"], "name='%s'"%(student_name))
+	
+	print sql_update("peeps_avg", "average", avg, "id=%d"%(temp[0]["id"]))
+	c.execute(sql_update("peeps_avg", "average", avg, "id=%d"%(temp[0]["id"])))
 
+print "student grades"
 classes = formatted_select(c, ["name", "code", "mark"],
 	["courses", "peeps"], "peeps.id = courses.id")
 
@@ -41,8 +50,11 @@ students = formatted_select(c, ["name", "id"], ["peeps"], "")
 students_and_avg = {}
 for student in students:
 	students_and_avg[student["id"]] = get_average(c, student["name"])
+print ""
 
+print "dictionary of id and averages"
 print students_and_avg
+print ""
 
 try:
 	c.execute("CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, average REAL);")
@@ -55,8 +67,30 @@ try:
 except sqlite3.OperationalError:
 	pass
 
-c.execute(sql_update("peeps_avg", "average", 25, "id=5"))
+print "student name, ids, and averages"
+vals = formatted_select(c, ["name", "peeps.id", "average"],
+	["peeps", "peeps_avg"], "peeps.id=peeps_avg.id")
+for v in vals:
+	print "id: %d, name: %s, average: %d"%(v["peeps.id"],
+		v["name"], v["average"])
+
+
+print "adding courses"
+c.execute(sql_insert_into("courses",
+	create_value_string(["id", "code", "mark"]),
+	create_value_string(["1", "gym", "75"])))
+
+c.execute(sql_insert_into("courses",
+	create_value_string(["id", "code", "mark"]),
+	create_value_string(["1", "ap5 chemistry", "0"])))
+
+print "updating average"
+update_average(c, "kruder")
+
+
+#c.execute(sql_update("peeps_avg", "average", 25, "id=5"))
 
 
 db.commit()
 db.close()
+
